9977a72d95f79bc04de4befc1506c0fb8c10d27b
Gott Blitz Official <gottblitzofficial@gmail.com>
2025-06-05 21:50:15 +0000

    2 Agrega el SDK de Firebase
    Usar npm
    
    Usar una etiqueta <script>
    Si ya

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,20 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getStorage, type FirebaseStorage } from "firebase/storage";
 
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCfcRi6GamHFCr_hkOWYQc1XQpM7pIWJ8U",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "click-shop-catalog.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "click-shop-catalog",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "click-shop-catalog.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "935571058219",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:935571058219:web:3f6880e2e9ed50833c046e",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID // Optional
 };
 
 // Initialize Firebase
@@ -32,6 +32,5 @@ if (!getApps().length) {
 auth = getAuth(app);
 db = getFirestore(app);
 storage = getStorage(app);
-
 
 export { app, auth, db, storage };
